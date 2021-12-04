150b94c407eadfbab7c115e912aa916368b5df05
L-Carlos <luiscarlosfirminopinheiro@gmail.com>
2021-12-03 21:34:18 -0300

    added postgres database connection

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/L-Carlos/phone-number-normalizer
 
 go 1.17
+
+require github.com/lib/pq v1.10.4 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,58 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
+	"fmt"
+	"log"
 	"regexp"
+
+	_ "github.com/lib/pq"
 )
+
+const (
+	host    = "localhost"
+	port    = 5432
+	user    = "postgres"
+	pasword = "password-here"
+	dbname  = "phone_normalizer"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable",
+		host, port, user, pasword)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	checkErr(err)
+
+	err = resetDB(db, dbname)
+	checkErr(err)
+
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	checkErr(err)
+
+	defer db.Close()
+
+	checkErr(db.Ping())
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
 
 func normalize(phone string) string {
 	var buf bytes.Buffer
@@ -21,6 +71,8 @@ func normalizeRegex(phone string) string {
 	return re.ReplaceAllString(phone, "")
 }
 
-func main() {
-
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
